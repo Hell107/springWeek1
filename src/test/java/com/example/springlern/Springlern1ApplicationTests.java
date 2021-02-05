7a992e0e5e0c116b599370722b8511242cb7a43f
hellp <hell.parahnya@mail.ru>
2021-02-05 22:11:55 +0300

    Initial commit

---
+++ src/test/java/com/example/springlern/Springlern1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springlern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springlern1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
